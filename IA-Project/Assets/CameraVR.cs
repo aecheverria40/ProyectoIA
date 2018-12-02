91d094e5bf1fce2b42c14eb8a796db95495c2b4f
msiqueiros <35547779+msiqueiros@users.noreply.github.com>
2018-12-02 15:59:18 -0800

    Fixed menu gyroscope.
    Changed render mode for canvas to world and added GvrEditorEmulator to Menu scene

--- IA-Project/Assets/CameraVR.cs
+++ IA-Project/Assets/CameraVR.cs
@@ -3,15 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.VR;
 
-public class CameraVR : MonoBehaviour {
+public class CameraVR : MonoBehaviour
+{
     [SerializeField] private float m_RenderScale = 1f;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+
+    private bool gyroEnabled;
+    private Gyroscope gyroscope;
+    private GameObject cameraContainer;
+    private Quaternion rot;
+
+    void Start()
+    {
         UnityEngine.XR.XRSettings.eyeTextureResolutionScale = m_RenderScale;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        cameraContainer = new GameObject("Camera Container");
+        cameraContainer.transform.position = transform.position;
+        transform.SetParent(cameraContainer.transform);
+        gyroEnabled = EnableGyro();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gyroEnabled)
+        {
+            transform.localRotation = gyroscope.attitude * rot;
+        }
+    }
+    private bool EnableGyro()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyroscope = Input.gyro;
+            gyroscope.enabled = true;
+            cameraContainer.transform.rotation = Quaternion.Euler(90f, 90f, 0f);
+            rot = new Quaternion(0, 0, 1, 0);
+            return true;
+        }
+        return false;
+    }
+
+
 }
